0810aab077596f326f371eec7f5583004d095a81
sayed shahid <shahidclt93@gmail.com>
2025-02-11 20:28:16 +0530

    cors frontend link added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -129,8 +129,8 @@ export const login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
-        // httpOnly: true,
-        sameSite: "None",
+        httpOnly: true,
+        sameSite: "Strict",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
