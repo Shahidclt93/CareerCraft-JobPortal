323ede58d4cabdaf6f26e486bc80ca99c70eb074
sayed shahid <shahidclt93@gmail.com>
2025-02-11 19:52:48 +0530

    cookie error

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -129,8 +129,8 @@ export const login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: "Lax",
+        // httpOnly: true,
+        sameSite: "None",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
