c4f8d52dbe43ab99f7d5298cbaf502d9dfa44e95
sayed shahid <shahidclt93@gmail.com>
2025-02-11 19:36:41 +0530

    cookie configuer change

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,3 +1,8 @@
+// export const USER_API_ENDPOINT = "http://localhost:4000/api/user";
+// export const JOB_API_ENDPOINT = "http://localhost:4000/api/job";
+// export const APPLICATION_API_ENDPOINT = "http://localhost:4000/api/application";
+// export const COMPANY_API_ENDPOINT = "http://localhost:4000/api/company";
+
 export const USER_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/user";
 export const JOB_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/job";
 export const APPLICATION_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/application";
