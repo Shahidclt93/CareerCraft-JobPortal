72f3da204786d19988a6c73afa0896afc6249646
sayed shahid <shahidclt93@gmail.com>
2025-02-11 20:07:01 +0530

    cookie error fix

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// const corsOptions = {
-//   origin: ["http://localhost:5173"],
-//   credentials: true,
-// };
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
