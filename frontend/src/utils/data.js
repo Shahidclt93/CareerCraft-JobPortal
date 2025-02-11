b9f3961a44a8e4dbb02b6935e3e977a283047689
sayed shahid <shahidclt93@gmail.com>
2025-02-11 23:01:15 +0530

    api err fix

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,9 +1,4 @@
-// export const USER_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/user";
-// export const JOB_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/job";
-// export const APPLICATION_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/application";
-// export const COMPANY_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/company";
-
-export const USER_API_ENDPOINT = "http://localhost:4000/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:4000/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:4000/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:4000/api/company";
+export const USER_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://careercraft-backend.onrender.com/api/company";
